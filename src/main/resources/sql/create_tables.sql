4008f72a01aa9f79a36dbdb9b36c96e503066ec9
chekalin <chekalin@star.intra.ispras.ru>
2026-02-22 15:20:46 +0300

    DataBase scheme updated

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -1,47 +1,50 @@
 CREATE TABLE IF NOT EXISTS "MediaType"
 (
     "id"   SERIAL PRIMARY KEY,
-    "name" TEXT
+    "name" TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "Movie"
 (
     "id"           SERIAL PRIMARY KEY,
-    "name"         TEXT,
-    "release_date" DATE CHECK ("release_date" >= '1900-01-01'),
-    "director"     TEXT,
-    "company"      TEXT
+    "name"         TEXT NOT NULL,
+    "release_date" DATE CHECK ("release_date" >= '1900-01-01') NOT NULL,
+    "director"     TEXT NOT NULL,
+    "company"      TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "Exemplar"
 (
     "id"         SERIAL PRIMARY KEY,
-    "movie_id"   INTEGER,
-    "media_type" INTEGER,
-    "available"  BOOLEAN,
+    "movie_id"   INTEGER NOT NULL,
+    "media_type_id" INTEGER NOT NULL,
+    "available"  BOOLEAN NOT NULL,
     CONSTRAINT "FK_Exemplar_movie_id"
         FOREIGN KEY ("movie_id")
             REFERENCES "Movie" ("id"),
     CONSTRAINT "FK_Exemplar_media_type"
-        FOREIGN KEY ("media_type")
+        FOREIGN KEY ("media_type_id")
             REFERENCES "MediaType" ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "Client"
 (
     "id"           SERIAL PRIMARY KEY,
-    "full_name"    TEXT,
+    "full_name"    TEXT NOT NULL,
     "address"      TEXT,
-    "phone_number" TEXT,
+    "phone_number" TEXT NOT NULL,
     "image_path"   TEXT
 );
+
+CREATE TYPE transaction_type AS ENUM ('issue', 'return');
 
 CREATE TABLE IF NOT EXISTS "Transaction"
 (
     "id"          SERIAL PRIMARY KEY,
-    "exemplar_id" INTEGER,
-    "client_id"   INTEGER,
-    "time"        TIMESTAMPTZ,
+    "exemplar_id" INTEGER NOT NULL,
+    "client_id"   INTEGER NOT NULL,
+    "time"        TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "type"        transaction_type NOT NULL,
     CONSTRAINT "FK_Transaction_exemplar_id"
         FOREIGN KEY ("exemplar_id")
             REFERENCES "Exemplar" ("id"),
@@ -49,3 +52,20 @@ CREATE TABLE IF NOT EXISTS "Transaction"
         FOREIGN KEY ("client_id")
             REFERENCES "Client" ("id")
 );
+
+CREATE VIEW "IssuedExemplars" AS
+SELECT C.id AS client_id, E.id AS exemplar_id
+FROM "Transaction" T
+         JOIN "Client" C on T.client_id = C.id
+         JOIN "Exemplar" E on T.exemplar_id = E.id
+WHERE E.available = FALSE
+  AND T.type = 'issue'
+  AND NOT EXISTS (SELECT 1
+                  FROM "Transaction" T2
+                  WHERE T2.exemplar_id = E.id
+                    AND T2.type = 'return'
+                    AND T2.time > T.time);
+
+SELECT exemplar_id
+FROM "IssuedExemplars"
+WHERE client_id = 1;
